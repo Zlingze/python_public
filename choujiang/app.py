a9274d9b2319f70084653854aac5608d89376b36
Zlingze <zq1594955li@163.com>
2024-08-20 18:03:05 +0800

    change

--- choujiang/app.py
+++ choujiang/app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+from random import randint
 
 app = Flask(__name__)
 
@@ -7,5 +8,10 @@ hero = ['黑暗之女', '狂战士', '正义巨像', '猩红收割者']
 @app.route('/index')
 def index():
     return render_template("index.html", hero=hero)
+
+@app.route('/choujiang')
+def choujiang():
+    num = randint(0, len(hero)-1)
+    return render_template("index.html", hero=hero, h=hero[num])
 
 app.run(debug=True)
